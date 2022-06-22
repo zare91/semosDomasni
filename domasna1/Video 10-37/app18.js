7c6f9d7d9cdcfbccd72849b01b263afe01ea3782
Zharko Petkovski <zharkopetkovski1991@gmail.com>
2022-06-22 19:35:53 +0200

    Domasna 1-3 edit

--- domasna1/Video 10-37/app18.js
+++ domasna1/Video 10-37/app18.js
@@ -12,5 +12,5 @@ var server = http.createServer(function(req, res){
     res.end(JSON.stringify(myObj));
 });
 
-server.listen (3000, '127.0.0.3');
+server.listen (3000, '127.0.0.1');
 console.log ('listening to port 3000');
